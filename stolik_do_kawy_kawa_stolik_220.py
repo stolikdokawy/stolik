18300f2c76feaeaacfa9d51c2446632a26933ef9
stolikdokawy <stolikdokawypanak@gmail.com>
2025-04-06 23:28:18 +0200

    Update stolik_do_kawy_kawa_stolik_220.py

--- stolik_do_kawy_kawa_stolik_220.py
+++ stolik_do_kawy_kawa_stolik_220.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 pygame.init()
-screen_game=pygame.display.set_mode((700,700))#ustawiamy wymiary ekranu gry
+screen_game=pygame.display.set_mode((700,700))
 screen_rect=screen_game.get_rect()#tworzymy prostokat ekranu
 limegreen = (50, 205, 50)
 black = (0, 0, 0)
@@ -22,33 +22,30 @@ while True:
             sys.exit()
             
     przycisk = pygame.key.get_pressed()#nacisniecie przycisku
-    if przycisk[pygame.K_d] and x < screen_rect.width - 100:
+    if przycisk[pygame.K_d] and x < screen_rect.width - 70:
         x=x+4
     if przycisk[pygame.K_a] and x > 0:
         x=x-4
     if przycisk[pygame.K_w] and y > 0:
         y=y-4
-    if przycisk[pygame.K_s] and y < screen_rect.height - 100:
+    if przycisk[pygame.K_s] and y < screen_rect.height - 70:
         y=y+4
 
     screen_game.fill(black)
-    kawa = pygame.Rect(x,y,100,100)
+    kawa = pygame.Rect(x,y,70,70)
     stolik = pygame.Rect(x_stolika,y_stolika,220,120)
     
     if kawa.colliderect(stolik):
         if przycisk[pygame.K_d]:
-            x = 140
+            x = 170
         if przycisk[pygame.K_a]:
             x = 460
         if przycisk[pygame.K_w]:
             pass
         if przycisk[pygame.K_s]:
-            y = 474
+            y = 504
             
-    kawa = pygame.Rect(x,y,100,100)
-    stolik = pygame.Rect(x_stolika,y_stolika,220,120)
-    pygame.draw.rect(screen_game, limegreen, kawa)
-    #pygame.draw.rect(screen_game, chocolate, stolik)
+    screen_game.blit(kawa_image, (x,y))
     screen_game.blit(stolik_image, (x_stolika,y_stolika))
     zegar.tick(60)#odswiezamy ekran po przesunieciu kubka
     pygame.display.update()
